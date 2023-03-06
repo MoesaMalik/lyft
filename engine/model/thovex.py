b988bd597c7a24a3e7f70b3c05b717f2a027b06a
MoesaMalik <114537446+MoesaMalik@users.noreply.github.com>
2023-03-06 14:43:38 -0500

    Update thovex.py

--- engine/model/thovex.py
+++ engine/model/thovex.py
@@ -1,12 +1,16 @@
-from datetime import datetime
+from car import Car
+from engines.capulet_engine import CapuletEngine
+from batteries.nubbin_battery import NubbinBattery
 
-from engine.capulet_engine import CapuletEngine
+class Thovex(Car):
+    def __init__(self, current_mileage, last_service_mileage, last_service_date):
+        thovex_engine = CapuletEngine(current_mileage, last_service_mileage)
+        thovex_battery = NubbinBattery(last_service_date)
 
+        super().__init__(thovex_engine, thovex_battery)
 
-class Thovex(CapuletEngine):
+        self.engine = thovex_engine
+        self.battery = thovex_battery
+
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+        return super().needs_service()
