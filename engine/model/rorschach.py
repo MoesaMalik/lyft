75dde21e16b6c697f3db740e05d3610ea8b8984f
MoesaMalik <114537446+MoesaMalik@users.noreply.github.com>
2023-03-06 14:43:24 -0500

    Update rorschach.py

--- engine/model/rorschach.py
+++ engine/model/rorschach.py
@@ -1,12 +1,16 @@
-from datetime import datetime
+from car import Car
+from engines.willoughby_engine import WilloughbyEngine
+from batteries.nubbin_battery import NubbinBattery
 
-from engine.willoughby_engine import WilloughbyEngine
+class Rorschach(Car):
+    def __init__(self, current_mileage, last_service_mileage, last_service_date):
+        rorschach_engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        rorschach_battery = NubbinBattery(last_service_date)
 
+        super().__init__(rorschach_engine, rorschach_battery)
 
-class Rorschach(WilloughbyEngine):
+        self.engine = rorschach_engine
+        self.battery = rorschach_battery
+
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+        return super().needs_service()
