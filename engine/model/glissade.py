fc8dbcb3f89bfcae1f4c38f183dbbcc31b29ff30
MoesaMalik <114537446+MoesaMalik@users.noreply.github.com>
2023-03-06 14:44:59 -0500

    Update glissade.py

--- engine/model/glissade.py
+++ engine/model/glissade.py
@@ -1,6 +1,6 @@
 from car import Car
 from engines.willoughby_engine import WilloughbyEngine
-from batteries.spindler_battery import SpindlerBattery
+from battery.spindler_battery import SpindlerBattery
 
 class Glissade(Car):
     def __init__(self, current_mileage, last_service_mileage, last_service_date):
